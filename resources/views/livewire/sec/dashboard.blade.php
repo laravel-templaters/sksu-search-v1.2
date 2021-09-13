e4fbc1e8e8d1bec36c79fc0a32adc7bb0a7fe71a
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-09-13 16:55:09 +0800

    dashboard thing

--- resources/views/livewire/sec/dashboard.blade.php
+++ resources/views/livewire/sec/dashboard.blade.php
@@ -5,7 +5,7 @@
         </div>
 
         <ul role="list" class="grid grid-cols-1 gap-3">
-        
+        {{-- loop here for type --}}
             <li class="flex flex-col col-span-1 text-left bg-white rounded-md shadow-sm" x-data="{showMe : false}">
                 <div class="flex justify-between min-w-full p-3" x-on:click="showMe = !showMe ">
                     <h3 class="my-auto text-xl font-extrabold text-primary-text">DISBURSEMENTS</h3>
@@ -16,43 +16,25 @@
                         </button>
                     </div>
                 </div>
-                {{-- content --}}
                 <div class="p-3 ml-3 bg-gray-200 rounded-md " x-cloak x-show="showMe">
-                   <div x-data="{openCA:false, openRe:false }">
+                {{-- loop category here --}}
+                   <div x-data="{openCA:false}">
                         <h3 class="p-2 text-black rounded-md text-md hover:bg-gray-600 hover:text-white" x-on:click="openCA = !openCA ">Cash Advances</h3>
                         <div class="grid flex-col grid-cols-1 p-3 ml-3 bg-gray-400 rounded-md " x-cloak x-show="openCA">
+                        {{-- sub cat only a --}}
                             <a href="{{route('cdv', ['id' => '2'])}}" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Local Travel</a>
+                        {{-- sub cat only a end--}}
                             <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Foreign Travel</a>
                             <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Activity, Program, Project, ETC.</a>
                             <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Payroll</a>
                             <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Special Disbursing Officer</a>
                             
                         </div>
-                        <h3 class="p-2 text-black rounded-md text-md hover:bg-gray-600 hover:text-white" x-on:click="openRe = !openRe ">Reimbursements</h3>
-                        <div class="grid flex-col grid-cols-1 p-3 ml-3 bg-gray-400 rounded-md " x-cloak x-show="openRe">
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Local Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Foreign Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Activity, Program, Project, ETC.</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Supplies/Materials</a>
-                            
-                        </div>
                     </div>
+                {{-- loop category here end--}}
                 </div>
             </li>
-
-            <li class="flex flex-col col-span-1 text-left bg-white rounded-md shadow-sm" x-data="{showMe : false}">
-                <div class="flex justify-between min-w-full p-3" x-on:click="showMe = !showMe ">
-                    <h3 class="my-auto text-xl font-extrabold uppercase text-primary-text">Travel Order</h3>
-                    <div class="flex">
-                       <button > <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 my-auto text-transparent text-primary-text" id="chevron" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
-                        </svg>
-                        </button>
-                    </div>
-                </div>
-            </li>
-
-            
+           {{-- loop here for type  end--}}
         </ul>
 
 </div>
