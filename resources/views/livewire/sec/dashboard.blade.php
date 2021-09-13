6a51cdbf4e9de8ad25cf6bb9a29dea9b2029aeab
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-09-13 16:50:24 +0800

    ey

--- resources/views/livewire/sec/dashboard.blade.php
+++ resources/views/livewire/sec/dashboard.blade.php
@@ -42,34 +42,12 @@
 
             <li class="flex flex-col col-span-1 text-left bg-white rounded-md shadow-sm" x-data="{showMe : false}">
                 <div class="flex justify-between min-w-full p-3" x-on:click="showMe = !showMe ">
-                    <h3 class="my-auto text-xl font-extrabold text-primary-text">Travel Order</h3>
+                    <h3 class="my-auto text-xl font-extrabold uppercase text-primary-text">Travel Order</h3>
                     <div class="flex">
                        <button > <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 my-auto text-transparent text-primary-text" id="chevron" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
                         </svg>
                         </button>
-                    </div>
-                </div>
-                {{-- content --}}
-                <div class="p-3 ml-3 bg-gray-200 rounded-md " x-cloak x-show="showMe">
-                   <div x-data="{openCA:false, openRe:false }">
-                        <h3 class="p-2 text-black rounded-md text-md hover:bg-gray-600 hover:text-white" x-on:click="openCA = !openCA ">Cash Advances</h3>
-                        <div class="grid flex-col grid-cols-1 p-3 ml-3 bg-gray-400 rounded-md " x-cloak x-show="openCA">
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Local Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Foreign Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Activity, Program, Project, ETC.</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Payroll</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Special Disbursing Officer</a>
-                            
-                        </div>
-                        <h3 class="p-2 text-black rounded-md text-md hover:bg-gray-600 hover:text-white" x-on:click="openRe = !openRe ">Reimbursements</h3>
-                        <div class="grid flex-col grid-cols-1 p-3 ml-3 bg-gray-400 rounded-md " x-cloak x-show="openRe">
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Local Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Foreign Travel</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Activity, Program, Project, ETC.</a>
-                            <a href="" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">Supplies/Materials</a>
-                            
-                        </div>
                     </div>
                 </div>
             </li>
