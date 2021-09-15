c86eb6ea8e564570262990c66c53b2649b4122ac
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-09-15 08:51:36 +0800

    fixed some shit

--- app/Http/Livewire/Sec/Dashboard.php
+++ app/Http/Livewire/Sec/Dashboard.php
@@ -9,11 +9,38 @@ use App\Models\DVSubCategory;
 
 class Dashboard extends Component
 {
+    public $dv_type_id;
+    public $dv_category_id;
+    Public $ctr = 0;
+    public $categories;
+    public $sub_categories;
     public function render()
     {
         $types = DVType::get();
-        $categories = DVCategory::get();
-        $sub_categories = DVSubCategory::get();
-        return view('livewire.sec.dashboard')->with('types', $types)->with('categories', $categories)->with('sub_categories', $sub_categories);
+        // $categories = DVCategory::get();
+        // $sub_categories = DVSubCategory::get();
+        // return view('livewire.sec.dashboard')->with('types', $types)->with('categories', $categories)->with('sub_categories', $sub_categories);
+        // return view('livewire.sec.dashboard')->with('types', $types);
+        if(isset($dv_type_id)){
+            
+            if(isset($dv_category_id)){
+               
+                $categories = DVCategory::where('dv_type_id', '=', $dv_type_id)->get();
+                $sub_categories = DVSubCategory::where('dv_category_id', '=', $dv_category_id)->get();
+                return view('livewire.sec.dashboard')->with('types', $types)->with('categories', $categories)->with('sub_categories', $sub_categories);
+               
+
+            }
+            else{
+                
+                $categories = DVCategory::where('dv_type_id', '=', $dv_type_id)->get();
+                return view('livewire.sec.dashboard')->with('types', $types)->with('categories', $categories);
+            }
+
+        }else{
+            
+            return view('livewire.sec.dashboard')->with('types', $types);
+
+        }
     }
 }
--- resources/views/livewire/sec/dashboard.blade.php
+++ resources/views/livewire/sec/dashboard.blade.php
@@ -7,8 +7,7 @@
         <ul role="list" class="grid grid-cols-1 gap-3">
         {{-- loop here for type --}}
         @foreach ($types as $type)
-            
-        
+           <h3 class="hidden">{{Illuminate\Support\Facades\Hash::make('aasd'.$dv_type_id = $type->id)}}</h3>
             <li class="flex flex-col col-span-1 text-left bg-white rounded-md shadow-sm" x-data="{showMe : false}">
                 <div class="flex justify-between min-w-full p-3" x-on:click="showMe = !showMe ">
                     <h3 class="my-auto text-xl font-extrabold text-primary-text">{{$type->dv_type}}</h3>
@@ -21,18 +20,29 @@
                 </div>
                 <div class="p-3 ml-3 bg-gray-200 rounded-md " x-cloak x-show="showMe">
                 {{-- loop category here --}}
-                @foreach ($categories as $category)
+                @php
+                if(isset($dv_type_id)){
+                    $categories = App\Models\DVCategory::where('dv_type_id', '=', $dv_type_id)->get();
+                }
+                @endphp
+                 @foreach ($categories as $category)
+                  <h3 class="hidden">{{Illuminate\Support\Facades\Hash::make('aasd'.$dv_category_id = $category->id)}}</h3>
                    <div x-data="{openCA:false}">
                         <h3 class="p-2 text-black rounded-md text-md hover:bg-gray-600 hover:text-white" x-on:click="openCA = !openCA ">{{$category->dv_category}}</h3>
                         <div class="grid flex-col grid-cols-1 p-3 ml-3 bg-gray-400 rounded-md " x-cloak x-show="openCA">
                         {{-- sub cat only a --}}
-                        @foreach ($sub_categories as $sub_category)
-                            <a href="{{route('cdv', ['id' => $sub_category->id])}}" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">{{$sub_category->dv_sub_category}}</a>
-                        {{-- sub cat only a end--}}
-                        @endforeach                            
+                        @php
+                            if(isset($dv_type_id)){
+                                $sub_categories = App\Models\DVSubCategory::where('dv_category_id', '=', $dv_category_id)->get();
+                            }
+                        @endphp
+                                    @foreach ($sub_categories as $sub_category)
+                                        <a href="{{route('cdv', ['id' => $sub_category->id])}}" class="p-2 text-black rounded-md text-md hover:bg-gray-800 hover:text-white">{{$sub_category->dv_sub_category}}</a>
+                                    {{-- sub cat only a end--}}
+                                    @endforeach 
                         </div>
                     </div>
-                    @endforeach   
+                    @endforeach 
                 {{-- loop category here end--}}
                 </div>
             </li>
@@ -41,3 +51,5 @@
         </ul>
 
 </div>
+
+ 
