698991ede461693fc518386ac056a54a0e36a335
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-09-16 20:36:40 +0800

    added travelorder

---
+++ app/Http/Livewire/Sec/To/TravelOrderMain.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Sec\To;
+
+use Livewire\Component;
+
+class TravelOrderMain extends Component
+{
+    public function render()
+    {
+        return view('livewire.sec.to.travel-order-main');
+    }
+}
---
+++ resources/views/travelorder/travel-order-wrapper.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+
+@livewire('sec.to.travel-order-main')
+
+</x-app-layout>
