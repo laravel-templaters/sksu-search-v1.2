54a647f17fc0690e57cb5f9a268027cfbc7879b0
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-11-23 18:40:46 +0800

    dv shit

--- database/seeders/DVCategorySeeder.php
+++ database/seeders/DVCategorySeeder.php
@@ -23,19 +23,83 @@ class DVCategorySeeder extends Seeder
                 'dv_type_id'=> 1,
             ]);
             DB::table('dv_categories')->insert([
-                'dv_category'=> 'Individual Compensation for Salary/Wage',
+                'dv_category'=> 'Individual Compensation for Salary/Wage (COS/JO)',
                 'dv_type_id'=> 1,
             ]);
             DB::table('dv_categories')->insert([
-                'dv_category'=> 'Payroll Compensation for Salaries/Wages',
+                'dv_category'=> 'Payroll Compensation for Salaries/Wages (COS/JO)',
                 'dv_type_id'=> 1,
             ]);
              DB::table('dv_categories')->insert([
-                'dv_category'=> 'asd',
+                'dv_category'=> 'Utilities, Fuel, Internet, Telephone, Etc.',
                 'dv_type_id'=> 1,
             ]);
             DB::table('dv_categories')->insert([
-                'dv_category'=> 'asd',
+                'dv_category'=> 'Payment to Contractors of Infrastructure Projects',
+                'dv_type_id'=> 1,
+            ]);
+            DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Compensation for PS (Overload/Overtime/Honorarium/Requested Subject/Others)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Payroll Compensation for PS (Overload/Overtime/Honorarium/Requested Subject/Others)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Compensation for Salary/Wage (Permanent/Temporary/Casual)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Regular Payroll for Salaries/Wages (Permanent/Temporary/Casual)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Compensation for Part-Time Services',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Payroll Compensation for Part-Time Services',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Salary/Wage (COS/JO/Laborer)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Regular Payroll for Salaries/Wages (COS/JO/Laborer)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Pay/Honorarium for External Experts/Professionals (Activity/Project/Program-Based)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Payroll Pay/Honorarium for External Experts/Professionals (Activity/Project/Program-Based)',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Compensation for Special Allowances and Bonuses',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Payroll Compensation for Special Allowances and Bonuses',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Individual Compensation for Laborers, Student Assistants, Etc.',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Payroll Compensation for Laborers, Student Assistants, Etc.',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Remittance of Payroll Deductions',
+                'dv_type_id'=> 1,
+            ]);
+             DB::table('dv_categories')->insert([
+                'dv_category'=> 'Remittance of Taxes Withheld',
                 'dv_type_id'=> 1,
             ]);
                 
