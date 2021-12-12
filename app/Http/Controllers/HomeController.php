4eb5260a21cd4c5a98ef1ba66afc109e0094e7cf
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-12-12 19:09:30 +0800

    fix?

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,49 +15,24 @@ class HomeController extends Controller
         if($role == 1)
         {
             return redirect()->route('dashboard');
-        }else if($role == 3){
-          // event(new ForwardDV(Auth::user(), 'hello gabriel'));
-           return redirect()->route('client-dash');
+            
         }else if($role == 2){
 
-                //temporary fix
-            if(Auth::user()->department->admin_user != null){
-                if (Auth::user()->department->admin_user->id == Auth::user()->id || Auth::user()->department->head_user->id == Auth::user()->id) {
-                    return redirect()->route('accountant-dashboard');
-                } else {
-                    return redirect()->route('department-head');
-                }
-                
-            }else{
-                return redirect()->route('department-head');
-            }
+            return redirect()->route('department-head');
         
+         }else if($role == 3){
+
+           return redirect()->route('client-dash');
+
          }else if($role == 4){
-                 //temporary fix
-            if(Auth::user()->department->admin_user != null){
-                if (Auth::user()->department->admin_user->id == Auth::user()->id || Auth::user()->department->head_user->id == Auth::user()->id) {
-                    
-                } else {
-                    return redirect()->route('accountant-dashboard');
-                }
-                
-            }else{
-                return redirect()->route('accountant-dashboard');
-            }
+              
+            return redirect()->route('accountant-dashboard');
            
          }
          else if($role == 5){
-            if(Auth::user()->department->admin_user != null){
-                if (Auth::user()->department->admin_user->id == Auth::user()->id || Auth::user()->department->head_user->id == Auth::user()->id) {
-                    
-                } else {
-                    return redirect()->route('budget-dashboard');
-                }
-                
-            }else{
-                return redirect()->route('budget-dashboard');
-            }
-           
+
+            return redirect()->route('budget-dashboard'); 
+                      
          }
     }
     public function test()
