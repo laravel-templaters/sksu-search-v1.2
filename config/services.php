74f91bdcd8afb98e5bfc35216f0b968f6ea43057
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-07-12 10:55:20 +0800

    hays

--- config/services.php
+++ config/services.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
 
     /*
@@ -32,7 +31,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://'.config('app_url'). '/auth/google/callback',
+        'redirect' => config('app_url'). '/auth/google/callback',
 
     ],
 
